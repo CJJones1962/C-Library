e96953fcb9674db303de2b8b30fa3053abd17430
CJJones1962 <cjjones1701a@gmail.com>
2017-08-15 19:11:35 -0700

    ChallengeStudentCourse 1.0

---
+++ ChallengeStudentCourses/ChallengeStudentCourses/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeStudentCourses.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="assignmentButton1" runat="server" OnClick="assignmentButton1_Click" Text="Assignment 1" />
+&nbsp;<br />
+        <br />
+        <asp:Button ID="assignmentButton2" runat="server" OnClick="assignmentButton2_Click" Text="Assignment 2" />
+&nbsp;<br />
+        <br />
+        <asp:Button ID="assignmentButton3" runat="server" OnClick="assignmentButton3_Click" Text="Assignment 3" />
+&nbsp;<br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+&nbsp;<br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeStudentCourses/ChallengeStudentCourses/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeStudentCourses
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void assignmentButton1_Click(object sender, EventArgs e)
+        {
+            List<Course> courses = new List<Course>() {
+                new Course() {CourseId = 1, Name = "English 101", Students = new List<Student>() {
+                    new Student () { StudentId = 1, Name = "CJ Carr"},
+                    new Student () { StudentId = 2, Name = "David Jones"},
+                    new Student () { StudentId = 3, Name = "Chris Jones" }}},
+                new Course() {CourseId = 2, Name = "Math 200", Students = new List<Student>() {
+                    new Student () { StudentId = 4, Name = "Frank James"},
+                    new Student () { StudentId = 5, Name = "Vanessa Harvey" },
+                    new Student () { StudentId = 6, Name = "Andrew Pine"}}},
+                new Course() {CourseId = 3, Name = "Art 102", Students = new List<Student>()  {
+                    new Student () { StudentId = 7, Name = "Pam Williams"},
+                    new Student () { StudentId = 8, Name = "Sam Elliot"},
+                    new Student () { StudentId = 9, Name = "Dwayne Johnson"}}}
+
+            };
+
+            foreach (var course in courses)
+            {
+                resultLabel.Text += string.Format("<br />Course: {0} - {1} - {2}", course.CourseId, course.Name);
+                foreach (var student in course.Students)
+                {
+                    resultLabel.Text += string.Format("<br />Student: {0} - {1} - {2}", student.StudentId, student.Name);
+                }
+            }
+        }
+            
+        protected void assignmentButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void assignmentButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
